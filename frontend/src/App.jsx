b8e39adc1aa8c44b606060038b545ae359d34fc2
vishwasvk35 <vishwasvk35@gmail.com>
2024-12-27 12:11:00 +0530

    initalize react and  tailwind setup in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='heading'>
+      <h1 className='text-red-500 text-5xl font-bold'>My React App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
